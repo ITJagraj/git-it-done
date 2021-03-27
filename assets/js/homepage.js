6ed9fc925acc8137a3ceab40cece47b0e5a14691
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-27 11:14:55 -0400

    initial files

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,11 @@
-var getUsersRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
-}
-getUsersRepos();
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+  };
